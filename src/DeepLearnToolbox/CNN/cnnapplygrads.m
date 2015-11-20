944b2f9f20e2c6345329942a75eb86828ce9b9f1
Liang Jiang <jiangliang0811@gmail.com>
2015-11-20 09:53:59 -0500

    calculate gradient

--- src/DeepLearnToolbox/CNN/cnnapplygrads.m
+++ src/DeepLearnToolbox/CNN/cnnapplygrads.m
@@ -6,15 +6,23 @@ net.gradient = 0;
                 for ii = 1 : numel(net.layers{l - 1}.a)
                     net.layers{l}.k{ii}{j} = net.layers{l}.k{ii}{j} - opts.alpha * net.layers{l}.dk{ii}{j};
                     gradient = net.layers{l}.dk{ii}{j};
+                    gradient = gradient .^ 2;
                     net.gradient = net.gradient + sum(gradient(:));
                 end
                 net.layers{l}.b{j} = net.layers{l}.b{j} - opts.alpha * net.layers{l}.db{j};
                 gradient = net.layers{l}.db{j};
+                gradient = gradient .^ 2;
                 net.gradient = net.gradient + sum(gradient(:));
             end
         end
     end
 
     net.ffW = net.ffW - opts.alpha * net.dffW;
+    gradient = net.dffW;
+    gradient = gradient .^ 2;
+    net.gradient = net.gradient + sum(gradient(:));
     net.ffb = net.ffb - opts.alpha * net.dffb;
+    gradient = net.dffb;
+    gradient = gradient .^ 2;
+    net.gradient = net.gradient + sum(gradient(:));
 end
