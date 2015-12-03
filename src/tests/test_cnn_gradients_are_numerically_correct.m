fc8722f3ed4355d07dcb661c42999ba0a40a6116
Liang Jiang <jiangliang0811@gmail.com>
2015-12-03 14:05:53 -0500

    modify cnntest

--- src/tests/test_cnn_gradients_are_numerically_correct.m
+++ src/tests/test_cnn_gradients_are_numerically_correct.m
@@ -1,6 +1,18 @@
-function test_cnn_gradients_are_numerically_correct
+addpath(genpath('../DeepLearnToolbox'));
 batch_x = rand(28,28,5);
-batch_y = rand(10,5);
+batch_y = randint(1,5, [1 10]);
+
+opts.alpha = 10e-5;
+opts.batchsize = 5;
+opts.numepochs = 30;
+opts.count = 1;
+opts.c = 500;
+opts.activation_type = 'relu';
+opts.row_size = 45;
+opts.col_size = 45;
+opts.num = 1024;
+opts.classes = 128
+
 cnn.layers = {
     struct('type', 'i') %input layer
     struct('type', 'c', 'outputmaps', 2, 'kernelsize', 5) %convolution layer
@@ -8,8 +20,8 @@ cnn.layers = {
     struct('type', 'c', 'outputmaps', 2, 'kernelsize', 5) %convolution layer
     struct('type', 's', 'scale', 2) %subsampling layer
 };
-cnn = cnnsetup(cnn, batch_x, batch_y);
+cnn = cnnsetup(cnn, batch_x, batch_y, opts);
 
-cnn = cnnff(cnn, batch_x);
-cnn = cnnbp(cnn, batch_y);
-cnnnumgradcheck(cnn, batch_x, batch_y);
+cnn = cnnff(cnn, batch_x, opts);
+cnn = cnnbp(cnn, batch_y, opts);
+cnnnumgradcheck(cnn, batch_x, batch_y, opts);
